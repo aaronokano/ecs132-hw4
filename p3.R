dc39de8bccf6c2c6879041360e0f8b63d67e900c
Anatoly Torchinsky <link13@gmail.com>
2012-11-13 11:01:11 -0800

    Added, nsamples started to work on p1 latex

--- p3.R
+++ p3.R
@@ -2,13 +2,13 @@ P3samples <- function(nsamples,p1,p2,p3) {
     X <- sum(sample(0:1,nsamples,replace=TRUE,prob=c(1-p1,p1)))
     Y <- sum(sample(0:1,nsamples,replace=TRUE,prob=c(1-p2,p2)))
     Z <- sum(sample(0:1,nsamples,replace=TRUE,prob=c(1-p3,p3)))
-    return( Z > 2*X*Y )
+    return( (Z/nsamples) > 2*(X/nsamples)*(Y/nsamples) )
 }
 
 P3 <- function(nreps) {
   f = vector( length=nreps )
   P <- sapply( f, function(x) P3samples(15,0.11,0.16,0.05) )
-  mean(P)
+  cat('Mean is ',mean(P), '\n')
 }
 
 P3( 10000 )

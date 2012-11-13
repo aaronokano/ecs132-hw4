cb9708735324962b38bfffc887d412ca169ce1be
Samuel Huang <samhuang91@gmail.com>
2012-11-12 16:02:49 -0800

    Added Answers.txt and modified p1,p3,questions.

--- p1.R
+++ p1.R
@@ -1,6 +1,13 @@
-ssq <- function(n) {
+ssq1 <- function(n) {
     # normalized population
     X <- rnorm(n, mean=0, sd=2)
+    # return variance
+    (1/n) * sum( (X - mean( X) )^2)
+}
+
+ssq2 <- function(n) {
+    # exponential population
+    X <- rexp(n)
     # return variance
     (1/n) * sum( (X - mean( X) )^2)
 }
@@ -9,9 +16,12 @@ P1 <- function(nreps) {
     # run for nreps repetitions
     tmp <- vector(length=nreps)
     # (n - 1)s^2/sigma^2 is chi-squared w/ 9 df
-    print(pchisq(9*4.8/4,df=9))
-    # see proportion of s^2 < 4.8
-    print(mean(sapply(tmp,function(x) {ssq(10)}) < 4.8))
+    chi <- pchisq(9*4.8/4,df=9)
+    # proportion of s^2 < 4.8
+    res1 <- mean(sapply(tmp,function(x) {ssq1(10)}) < 4.8)
+    res2 <- mean(sapply(tmp,function(x) {ssq2(10)}) < 4.8)
+    cat('Part A:\t',chi,'\t',res1,'\n')
+    cat('Part B:\t',res2,'\n')
 }
 
 P1(1000)
--- p3.R
+++ p3.R
@@ -2,14 +2,13 @@ P3samples <- function(nsamples,p1,p2,p3) {
     X <- sum(sample(0:1,nsamples,replace=TRUE,prob=c(1-p1,p1)))
     Y <- sum(sample(0:1,nsamples,replace=TRUE,prob=c(1-p2,p2)))
     Z <- sum(sample(0:1,nsamples,replace=TRUE,prob=c(1-p3,p3)))
-    #print(mean(Z > 2*X*Y))
     return( Z > 2*X*Y )
 }
 
 P3 <- function(nreps) {
   f = vector( length=nreps )
   P <- sapply( f, function(x) P3samples(15,0.11,0.16,0.05) )
-  length( P[ P ] ) / nreps
+  mean(P)
 }
 
 P3( 10000 )
